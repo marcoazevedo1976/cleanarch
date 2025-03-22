12d1a471668f6098f5c213917a81d0f2d6ad7f05
Marco Azevedo <marcoazevedo1976@gmail.com>
2025-03-22 13:41:53 -0400

    Configure GET Handler for Order

--- internal/infra/web/webserver/webserver.go
+++ internal/infra/web/webserver/webserver.go
@@ -7,22 +7,32 @@ import (
 	"github.com/go-chi/chi/v5/middleware"
 )
 
+type Handler struct {
+	Method      string
+	Path        string
+	HandlerFunc http.HandlerFunc
+}
+
 type WebServer struct {
 	Router        chi.Router
-	Handlers      map[string]http.HandlerFunc
+	Handlers      []Handler
 	WebServerPort string
 }
 
 func NewWebServer(serverPort string) *WebServer {
 	return &WebServer{
 		Router:        chi.NewRouter(),
-		Handlers:      make(map[string]http.HandlerFunc),
+		Handlers:      make([]Handler, 0),
 		WebServerPort: serverPort,
 	}
 }
 
-func (s *WebServer) AddHandler(path string, handler http.HandlerFunc) {
-	s.Handlers[path] = handler
+func (s *WebServer) AddHandler(method, path string, handler http.HandlerFunc) {
+	s.Handlers = append(s.Handlers, Handler{
+		Method:      method,
+		Path:        path,
+		HandlerFunc: handler,
+	})
 }
 
 // loop through the handlers and add them to the router
@@ -30,8 +40,8 @@ func (s *WebServer) AddHandler(path string, handler http.HandlerFunc) {
 // start the server
 func (s *WebServer) Start() {
 	s.Router.Use(middleware.Logger)
-	for path, handler := range s.Handlers {
-		s.Router.Handle(path, handler)
+	for _, handler := range s.Handlers {
+		s.Router.Method(handler.Method, handler.Path, handler.HandlerFunc)
 	}
 	http.ListenAndServe(s.WebServerPort, s.Router)
 }
--- internal/usecase/list_order.go
+++ internal/usecase/list_order.go
@@ -1,1 +1,34 @@
 package usecase
+
+import "github.com/marcoazevedo1976/cleanarch/internal/entity"
+
+type ListOrderUseCase struct {
+	OrderRepository entity.OrderRepositoryInterface
+}
+
+func NewListOrderUseCase(OrderRepository entity.OrderRepositoryInterface) *ListOrderUseCase {
+	return &ListOrderUseCase{
+		OrderRepository: OrderRepository,
+	}
+}
+
+func (l *ListOrderUseCase) Execute() ([]OrderOutputDTO, error) {
+	orders, err := l.OrderRepository.List()
+	if err != nil {
+		return nil, err
+	}
+
+	ordersOutputDTO := make([]OrderOutputDTO, 0)
+
+	for _, order := range orders {
+		dto := OrderOutputDTO{
+			ID:         order.ID,
+			Price:      order.Price,
+			Tax:        order.Tax,
+			FinalPrice: order.FinalPrice,
+		}
+		ordersOutputDTO = append(ordersOutputDTO, dto)
+	}
+
+	return ordersOutputDTO, nil
+}
